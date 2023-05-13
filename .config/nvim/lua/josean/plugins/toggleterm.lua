5bfe3921c7d5e85b40fac750a5c2ba25443e7d49
Lunovoy <anuilov1991@mail.ru>
2023-05-13 12:01:19 +0300

    configs cnanged

--- .config/nvim/lua/josean/plugins/toggleterm.lua
+++ .config/nvim/lua/josean/plugins/toggleterm.lua
@@ -15,15 +15,15 @@ toggleterm.setup({
   shell = "/bin/zsh",
 })
 
--- function _G.set_terminal_keymaps()
---   local opts = {buffer = 0}
---   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
---   vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
---   vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
---   vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
---   vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
---   vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
---   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
--- end
---
--- vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "kj", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
