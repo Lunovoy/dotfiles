467e94489460d73add34fec9bc5f31b24f3a744e
Mikhail Anuylov <ma@couch.ru>
2024-08-28 03:50:20 +0300

    add new plugins(center buf, lsp signature hint)

---
+++ .config/nvim/lua/hellsing/plugins/center-buffer.lua
@@ -0,0 +1,23 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+  version = "*",
+  config = function()
+    require("no-neck-pain").setup({
+      buffers = {
+        colors = {
+          background = "tokyonight-moon",
+        },
+        right = {
+          enabled = false,
+        },
+        wo = {
+          fillchars = "eob: ",
+        },
+      },
+    })
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+    keymap.set("n", "<leader>np", "<cmd>NoNeckPain<CR>", { desc = "Toggle center buffer" })
+  end,
+}
